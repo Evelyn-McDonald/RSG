255b11b78e56300cf1617a27659c25202e62596c
Evelyn-McDonald <evel.mcdonald@gmail.com>
2014-10-28 13:56:30 -0400

    jumbotron and top nav created

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Smooth Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+// Pause before fading badge in
+$(document).ready(
+  setTimeout(function() { 
+    $(".jumbotron h1").addClass("load"); 
+  }, 1000)
+);
