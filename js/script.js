0e64a5f46db882604be3f88c67f87e4f8e1c1fc2
Evelyn-McDonald <evel.mcdonald@gmail.com>
2014-11-02 16:07:27 -0500

    smooth scroll removed

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// Smooth Scrolling
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-
 // Pause before fading badge in
 $(document).ready(
   setTimeout(function() { 
